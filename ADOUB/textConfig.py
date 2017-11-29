e68b032e230cd97b7a2ffd11c52c447fd991fea8
dybl <dyblcjj@163.com>
2017-11-29 23:54:40 +0800

    Add files via upload

---
+++ ADOUB/textConfig.py
@@ -0,0 +1,11 @@
+"""
+config file for PyEdit source-code windows popped up by PyDemos here;
+PyEit imports this via normal sys.path, not package relative, so each
+client app can have it own configs;  no file or assignments = defaults;
+"""
+# initial font                      # family, size, style
+font = ('consolas', 12, 'bold')       # e.g., style: 'normal italic'
+
+# initial color                     # default=white, black
+bg = '#fef8cd'                      # colorname or RGB hexstr
+fg = 'black'                        # 'LightYellow', 'powder blue', '#690f96'
